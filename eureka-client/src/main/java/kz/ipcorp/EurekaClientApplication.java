46d6430da9a5bf86e71f1cbff805726da0bd752b
nursdev <stanbek1004@gmail.com>
2024-01-03 01:56:22 +0600

    init module eureka-client

--- eureka-client/src/main/java/kz/ipcorp/EurekaClientApplication.java
+++ eureka-client/src/main/java/kz/ipcorp/EurekaClientApplication.java
@@ -2,14 +2,14 @@ package kz.ipcorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+@EnableDiscoveryClient
+public class EurekaClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 
 }
