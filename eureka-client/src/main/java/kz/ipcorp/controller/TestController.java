ef88eeffc284041606364b4920dadcd2a0247453
nursdev <stanbek1004@gmail.com>
2024-01-05 02:58:17 +0600

    init api-gateway

--- eureka-client/src/main/java/kz/ipcorp/controller/TestController.java
+++ eureka-client/src/main/java/kz/ipcorp/controller/TestController.java
@@ -1,13 +1,21 @@
 package kz.ipcorp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String port;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return port;
     }
+
+    @GetMapping("/hello")
+    public String hello() { return "hello"; }
+
 }
