3d3ca29635bb57a5af8114ea9249dc7581aca457
nursdev <stanbek1004@gmail.com>
2024-01-14 04:30:38 +0600

    fix swagger, db

--- auth-service/src/main/java/kz/ipcorp/AuthServiceApplication.java
+++ auth-service/src/main/java/kz/ipcorp/AuthServiceApplication.java
@@ -1,7 +1,5 @@
 package kz.ipcorp;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
