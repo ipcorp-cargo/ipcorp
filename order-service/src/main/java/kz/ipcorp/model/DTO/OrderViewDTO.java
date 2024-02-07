f5b9991349af341f7ffae482dfd7ef9439c09032
nursdev <stanbek1004@gmail.com>
2024-02-08 00:39:11 +0600

    status list

--- order-service/src/main/java/kz/ipcorp/model/DTO/OrderViewDTO.java
+++ order-service/src/main/java/kz/ipcorp/model/DTO/OrderViewDTO.java
@@ -1,9 +1,11 @@
 package kz.ipcorp.model.DTO;
 
 import kz.ipcorp.model.entity.Order;
+import kz.ipcorp.model.entity.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -12,17 +14,23 @@ public class OrderViewDTO {
     private UUID id;
     private String trackCode;
     private String orderName;
-    private String status;
+    private List<String> statusList;
 
     public OrderViewDTO() {}
     public OrderViewDTO(Order order) {
         this.id = order.getId();
         this.trackCode = order.getTrackCode();
         this.orderName = order.getOrderName();
-        if(order.getStatus() != null) {
-            this.status = order.getStatus().getName();
-        }else {
-            this.status = "NULL";
+        List<OrderStatus> orderStatuses = order.getOrderStatuses();
+        if (orderStatuses != null) {
+            for (OrderStatus orderStatus : orderStatuses) {
+                statusList.add(orderStatus.getStatus().getName());
+            }
         }
+//        if(order.getStatus() != null) {
+//            this.status = order.getStatus().getName();
+//        }else {
+//            this.status = "NULL";
+//        }
     }
 }
